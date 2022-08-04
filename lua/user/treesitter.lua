657b0bad0dea826ed17cbdd4f5a8ac599f489169
KostasLampros <kostas_la@hotmail.gr>
2022-08-04 21:34:33 +0300

    Treesitter fix

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,18 +1,34 @@
-local status_ok, configs = pcall(require, "nvim-treesitter,configs")
-if not status_ok then
-    return
-end
+-- local status_ok, configs = pcall(require, "nvim-treesitter,configs")
+-- if not status_ok then
+    -- return
+-- end
 
-configs.setup {
-  ensure_installed = "maintained",
-  sync_install = false,
+require'nvim-treesitter.configs'.setup{
+  -- ensure_installed = "maintained",
+  -- sync_install = false,
   ensure_installed = { "c", "lua" },
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  -- ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-
+    -- disable = { "" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = false,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = {
+      enable = true,
+      disable = { "yaml", "python" }
+  },
+  rainbow = {
+      enable = true,
+      colors = {
+        -- "#68a0b0",
+        -- "#946EaD",
+        -- "#c7aA6D",
+        "Gold",
+        "Orchid",
+        "DodgerBlue",
+        -- "Cornsilk",
+        -- "Salmon",
+        -- "LawnGreen",
+      },
+    },
 }
