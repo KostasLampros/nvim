ae9c4803600cbb9bc60e5c77bdb05e79cd4f0463
KostasLampros <kostas_la@hotmail.gr>
2022-08-04 22:01:53 +0300

    plugin.lua added packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,5 +1,9 @@
 local fn = vim.fn
-
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -19,6 +23,9 @@ return packer.startup(function(use)
     use 'preservim/nerdcommenter'
     use 'windwp/nvim-autopairs'
     use 'ryanoasis/vim-devicons'
+    use 'wbthomason/packer.nvim'
+
+
     -- Colorschemes
     use 'gruvbox-community/gruvbox'
 
@@ -36,11 +43,13 @@ return packer.startup(function(use)
     -- LSP
     use 'neovim/nvim-lspconfig'
     use 'williamboman/nvim-lsp-installer'
+
     -- TreeSitter
     use {'nvim-treesitter/nvim-treesitter', run = 'TSUpdate',}
+
     -- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
+	if packer_bootstrap then
 		require("packer").sync()
 	end
 end)
